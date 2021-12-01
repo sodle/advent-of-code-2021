35bbf7fc775fd135c45e915c45279699c2263178
Scott Odle <scott@sjodle.com>
2021-12-01 09:59:10 -0800

    Benchmarks!

--- day01/main_test.go
+++ day01/main_test.go
@@ -26,3 +26,19 @@ func TestPart2(t *testing.T) {
 		t.Errorf("Day 00 Part 2 incorrect: Expected %d, got %d", want, got)
 	}
 }
+
+func BenchmarkPart1(b *testing.B) {
+	input := shared.ReadNumberFile("input.txt")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Part1(input)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	input := shared.ReadNumberFile("input.txt")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Part2(input)
+	}
+}
