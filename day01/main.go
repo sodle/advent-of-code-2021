9ee5380f191aef5155b39672bb62429129c7a7f8
Scott Odle <scott@sjodle.com>
2021-11-30 23:37:48 -0800

    Day 1

---
+++ day01/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"aoc2021/shared"
+	"log"
+)
+
+func Part1(depths []int) int {
+	depthIncreases := 0
+	for i, depth := range depths {
+		if i > 0 {
+			if depth > depths[i - 1] {
+				depthIncreases++
+			}
+		}
+	}
+	return depthIncreases
+}
+
+func Part2(depths []int) int {
+	var windowSums []int
+	depthIncreases := 0
+
+	for i, depth := range depths {
+		if i >= 2 {
+			windowSum := depth + depths[i-1] + depths[i-2]
+
+			countPreviousSums := len(windowSums)
+			if countPreviousSums > 0 {
+				if windowSum > windowSums[countPreviousSums-1] {
+					depthIncreases++
+				}
+			}
+
+			windowSums = append(windowSums, windowSum)
+		}
+	}
+	return depthIncreases
+}
+
+func main() {
+	numbers := shared.ReadNumberFile("day01/input.txt")
+	log.Printf("Part 1: %d\n", Part1(numbers))
+	log.Printf("Part 2: %d\n", Part2(numbers))
+}
