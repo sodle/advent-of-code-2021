9c7065e002b2576577dad2b239700c840e130a64
Scott Odle <scott@sjodle.com>
2021-11-30 23:37:48 -0800

    Day 1

---
+++ day01/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"aoc2021/shared"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	input := shared.ReadNumberFile("test_input.txt")
+
+	want := 7
+	got := Part1(input)
+
+	if want != got {
+		t.Errorf("Day 00 Part 1 incorrect: Expected %d, got %d", want, got)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := shared.ReadNumberFile("test_input.txt")
+
+	want := 5
+	got := Part2(input)
+
+	if want != got {
+		t.Errorf("Day 00 Part 2 incorrect: Expected %d, got %d", want, got)
+	}
+}
