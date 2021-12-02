0151cc1d286ad2dd567a6fe79339d29782633fae
Scott Odle <scott@sjodle.com>
2021-12-02 04:42:19 -0800

    Day 2

--- day02/main_test.go
+++ day02/main_test.go
@@ -6,29 +6,29 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	input := shared.ReadNumberFile("test_input.txt")
+	input := shared.ReadVectorsFromFile("test_input.txt")
 
-	want := 7
+	want := 150
 	got := Part1(input)
 
 	if want != got {
-		t.Errorf("Day 01 Part 1 incorrect: Expected %d, got %d", want, got)
+		t.Errorf("Day 02 Part 1 incorrect: Expected %d, got %d", want, got)
 	}
 }
 
 func TestPart2(t *testing.T) {
-	input := shared.ReadNumberFile("test_input.txt")
+	input := shared.ReadVectorsFromFile("test_input.txt")
 
-	want := 5
+	want := 900
 	got := Part2(input)
 
 	if want != got {
-		t.Errorf("Day 01 Part 2 incorrect: Expected %d, got %d", want, got)
+		t.Errorf("Day 02 Part 2 incorrect: Expected %d, got %d", want, got)
 	}
 }
 
 func BenchmarkPart1(b *testing.B) {
-	input := shared.ReadNumberFile("input.txt")
+	input := shared.ReadVectorsFromFile("input.txt")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Part1(input)
@@ -36,7 +36,7 @@ func BenchmarkPart1(b *testing.B) {
 }
 
 func BenchmarkPart2(b *testing.B) {
-	input := shared.ReadNumberFile("input.txt")
+	input := shared.ReadVectorsFromFile("input.txt")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Part2(input)
---
+++ shared/vectors.go
@@ -0,0 +1,63 @@
+package shared
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Vector struct {
+	X int
+	Y int
+}
+
+func forward(magnitude int) Vector {
+	return Vector{
+		magnitude,
+		0,
+	}
+}
+
+func backward(magnitude int) Vector {
+	return Vector{
+		-magnitude,
+		0,
+	}
+}
+
+func up(magnitude int) Vector {
+	return Vector{
+		X: 0,
+		Y: -magnitude,
+	}
+}
+
+func down(magnitude int) Vector {
+	return Vector{
+		X: 0,
+		Y: magnitude,
+	}
+}
+
+func ReadVectorsFromFile(path string) (out []Vector) {
+	lines := ReadLinesFromFile(path)
+	for _, line := range lines {
+		tokens := strings.Split(line, " ")
+		direction := tokens[0]
+		magnitude, err := strconv.Atoi(tokens[1])
+		if err != nil {
+			panic(err)
+		}
+
+		switch direction {
+		case "forward":
+			out = append(out, forward(magnitude))
+		case "backward":
+			out = append(out, backward(magnitude))
+		case "up":
+			out = append(out, up(magnitude))
+		case "down":
+			out = append(out, down(magnitude))
+		}
+	}
+	return out
+}
