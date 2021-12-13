2f8a5589e4b63024af3085c39ed8bfba8230fdfb
Scott Odle <scott@sjodle.com>
2021-12-12 20:05:03 -0800

    no idea how to solve day 12 yet

---
+++ day12/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"aoc2021/shared"
+	"log"
+)
+
+func Part1(lines []string) int {
+	return 0
+}
+
+func Part2(lines []string) int {
+	return 0
+}
+
+func main() {
+	numbers := shared.ReadLinesFromFile("day11/input.txt")
+	log.Printf("Part 1: %d\n", Part1(numbers))
+	log.Printf("Part 2: %d\n", Part2(numbers))
+}
